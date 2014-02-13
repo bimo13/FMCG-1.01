71b08702d5b9111dff1a29cfa3557a6b7c4c144d
bimo13 <bimo.jalankaki@yahoo.com>
2014-02-13 13:21:13 +0700

    first commit

--- www/includes/js/func-barcodeScanner.js
+++ www/includes/js/func-barcodeScanner.js
@@ -1,34 +1,43 @@
 var app = {
-    initialize: function() {
-        this.bindEvents();
-    },
-    
-	bindEvents: function() {
-        document.getElementById('button_scan_barcode').addEventListener('click', this.scan, false);
-    },
+	initialize: function(){
+		this.bindEvents();
+	},
 	
-	scan: function() {
-        console.log('scanning');
-        
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-        scanner.scan( function (result) { 
-
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        } );
-    }
-
+	bindEvents: function(){
+		document.getElementById('button_scan_barcode').addEventListener('click', this.scan, false);
+	},
+	
+	scan: function(){
+		console.log('scanning');
+		
+		var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+		
+		scanner.scan(function(result){
+			
+			/*
+			alert(
+				"We got a barcode\n" + 
+				"Result: " + result.text + "\n" + 
+				"Format: " + result.format + "\n" + 
+				"Cancelled: " + result.cancelled
+			);
+			*/
+			
+			var scan_result		=	result.text+"<br />"+result.format+"<br />"+result.cancelled
+			
+			$("#err_alert_text").empty();
+			$("#err_alert_text").append(scan_result);
+			$("#errordialog").modal();
+			
+			console.log(
+				"Scanner result: \n" +
+				"text: " + result.text + "\n" +
+				"format: " + result.format + "\n" +
+				"cancelled: " + result.cancelled + "\n"
+			);
+			console.log(result);
+		}, function(error) {
+			console.log("Scanning failed: ", error); 
+		});
+	}
 };
