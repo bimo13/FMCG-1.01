84a5c173bf278fe02d220159244ec93869dc0b0f
bimo13 <bimo.jalankaki@yahoo.com>
2014-02-13 14:58:31 +0700

    first commit

--- www/includes/js/func-barcodeScanner.js
+++ www/includes/js/func-barcodeScanner.js
@@ -23,12 +23,36 @@ var app = {
 			);
 			*/
 			
-			var scan_result		=	result.text+"<br />"+result.format+"<br />"+result.cancelled
-			
 			if(result.cancelled === false){
-				$("#err_alert_text").empty();
-				$("#err_alert_text").append(scan_result);
-				$("#errordialog").modal();
+				
+				var text_res		=	result.text;
+				var text_arr		=	text_res.split("|");
+				
+				$("#toko_nama").val(text_arr[1]);
+				$("#toko_alamat").val(text_arr[2]);
+				$("#pemilik_nama").val(text_arr[3]);
+				$("#pemilik_alamat").val(text_arr[5]);
+				$("#pemilik_telp").val(text_arr[6]);
+				$("#pemilik_mail").val(text_arr[7]);
+				
+				/*
+				$("#toko_id").attr('disabled',true);
+				$("#button_scan_barcode").attr('disabled',true);
+				$("#button_konfirmasi_toko").attr('disabled',true);
+				
+				$("#trx_toko_id").val(text_arr[1]);
+				$("#trx_data_id").val(text_arr[1]);
+				$("#button_del_trx").removeClass('hide');
+				*/
+				
+				if(text_arr[4] == "L"){
+					$("#pemilik_kelamin_l").attr("checked",true).checkboxradio("refresh");
+					$("#pemilik_kelamin_p").attr("checked",false).checkboxradio("refresh");
+				}else if(text_arr[4] == "P"){
+					$("#pemilik_kelamin_p").attr("checked",true).checkboxradio("refresh");
+					$("#pemilik_kelamin_l").attr("checked",false).checkboxradio("refresh");
+				}
+				
 			}else{
 				$("#err_alert_text").empty();
 				$("#err_alert_text").append("<i>Barcode Scan</i> dibatalkan.");
