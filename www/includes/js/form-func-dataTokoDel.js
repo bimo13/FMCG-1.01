edce05463896ed2700a34add1c05d22f18d01830
bimo13 <bimo.jalankaki@yahoo.com>
2014-02-14 15:06:44 +0700

    first commit

--- www/includes/js/form-func-dataTokoDel.js
+++ www/includes/js/form-func-dataTokoDel.js
@@ -25,20 +25,18 @@ function delete_dataToko(){
 				$("#pemilik_alamat").val("");
 				$("#pemilik_telp").val("");
 				$("#pemilik_mail").val("");
-				
 				$("#trx_toko_id").val("");
 				$("#trx_data_id").val("");
 				
 				$("#pleasewait").modal('hide');
 				
-				$("#pemilik_kelamin_p").attr("checked",false).checkboxradio("refresh");
-				$("#pemilik_kelamin_l").attr("checked",false).checkboxradio("refresh");
-				
 				$("#toko_id").attr('disabled',false);
 				$("#button_scan_barcode").attr('disabled',false);
 				$("#button_konfirmasi_toko").attr('disabled',false);
-				
 				$("#button_del_trx").addClass('hide');
+				
+				$("#pemilik_kelamin_p").attr("checked",false).checkboxradio("refresh");
+				$("#pemilik_kelamin_l").attr("checked",false).checkboxradio("refresh");
 			}else{
 				$("#pleasewait").modal('hide');
 				$("#err_alert_text").empty();
